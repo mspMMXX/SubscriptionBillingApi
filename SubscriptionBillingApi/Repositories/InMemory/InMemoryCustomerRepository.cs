3a5be65f5d140fc6d0e93f8c289b588ea03e2527
mspMMXX <post@msplatter.at>
2025-12-19 10:29:41 +0100

    Initial commit

---
+++ SubscriptionBillingApi/Repositories/InMemory/InMemoryCustomerRepository.cs
@@ -0,0 +1,31 @@
+﻿using SubscriptionBillingApi.Domain.Entities;
+using SubscriptionBillingApi.Repositories.Interfaces;
+
+namespace SubscriptionBillingApi.Repositories.InMemory
+{
+    public class InMemoryCustomerRepository : ICustomerRepository
+    {
+
+        private readonly Dictionary<Guid, Customer> _customer = new();
+
+        public Task AddAsync(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Customer>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer?> GetByIdAsync(Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
