ec56bd64f65c31d6c9fc03edd0b80a0036d2e6cd
mspMMXX <post@msplatter.at>
2025-12-20 10:06:29 +0100

    Add DI

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SubscriptionBillingApi.Services;
+
+namespace SubscriptionBillingApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly CustomerService _customerService;
+
+        public CustomersController(CustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var customers = await _customerService.GetAllCustomersAsync();
+            return Ok(customers);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
 using SubscriptionBillingApi.Services;
+using SubscriptionBillingApi.Repositories.Interfaces;
+using SubscriptionBillingApi.Repositories.InMemory;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddScoped<ICustomerRepository, InMemoryCustomerRepository>();
+builder.Services.AddScoped<ISubscriptionPlanRepository, InMemorySubscriptionPlanRepository>();
+builder.Services.AddScoped<ISubscriptionRepository, InMemorySubscriptionRepository>();
+builder.Services.AddScoped<IInvoiceRepository, InMemoryInvoiceRepository>();
+
 builder.Services.AddScoped<CustomerService>();
-
 builder.Services.AddScoped<SubscriptionService>();
+builder.Services.AddScoped<InvoiceService>();
+builder.Services.AddScoped<SubscriptionPlanService>();
 
-// Learn more about configuring Swagger/OpenAPI atOk https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at Ok https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
